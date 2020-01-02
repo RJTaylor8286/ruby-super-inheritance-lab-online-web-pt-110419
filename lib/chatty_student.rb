dcbdc314dc5031693e8e86424715f5fcc5fcb0ce
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2020-01-02 16:25:52 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,15 +2,14 @@ class ChattyStudent < Student
   def hello
     @hello = "Hey there! I'm so excited to learn stuff."
     @hello = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
-    super
+  
     @hello = true
   end
 end
 
   def raise_hand
     @raise_hand = "Pick me!"
-    super
-    
     super(10)
+    
   end
   
